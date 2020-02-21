92bed7714ca275e0c85f3e705624dd232bf17b59
Student <student@lab-311-033.cs.csubak.edu>
2020-02-21 09:45:53 -0800

    Melanie: Friday quiz, Menu update

--- melanieC.cpp
+++ melanieC.cpp
@@ -1,6 +1,17 @@
-//Melanie Corral
-//Credit implementation started
+//Class: Software Engineering 3350
+//Game_name: Cute as Hell
 //
+//Program: melanieC.cpp
+//Author: Melanie Corral
+//Date: 2020
+//Last modified 21 Feb 2020
+//
+//Completed:
+//Credit implementation
+//To do list:
+//Enemy AI attack/health
+//Different Enemy types
+
 #include <iostream>
 #include "fonts.h"
 
@@ -13,3 +24,16 @@ void MC_Credits(int yres)
         r.center = 0;
         ggprint8b(&r, 16, c, "Melanie Corral");
 }
+void Main_Menu(int yres)
+{
+    	Rect r;
+	unsigned int c = 0x00ffff00;
+
+	r.bot = yres - 165;
+	r.left = 275;
+	r.center = 0;
+        ggprint16(&r, 16, c, "Menu Test");
+
+
+}
+
