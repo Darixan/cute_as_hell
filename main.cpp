203e3ceee94bdb523686f42269b59661ac4713b0
mcorral <mcorral@DESKTOP-PU84940.localdomain>
2020-05-03 19:08:37 -0700

    melanie: added second enemy

--- main.cpp
+++ main.cpp
@@ -336,7 +336,9 @@ Bullet plBullet(5, playerPos, bulletVel);
 //Enemy dps[numEnemiesOnScreen];
 //Enemy healers[numEnemiesOnScreen];
 Vec enemyPos = {g.xres/1.5, g.yres/2.0, 0.0};
+Vec enemyPos1 = {g.xres/3.5, g.yres/1.0, 0.0};
 Enemy enemy(50, 25, enemyPos);
+Enemy enemy1(50, 25, enemyPos1);
 
 
 //****Platform Class Instantiation*****
@@ -589,36 +591,6 @@ void checkMouse(XEvent *e)
 	if (e->type == ButtonPress) {
 		if (e->xbutton.button==1) {
 			//Left button is down
-			//a little time between each bullet
-			struct timespec bt;
-			clock_gettime(CLOCK_REALTIME, &bt);
-			double ts = timeDiff(&g.bulletTimer, &bt);
-			if (ts > 0.1) {
-				timeCopy(&g.bulletTimer, &bt);
-				//shoot a bullet...
-				if (g.nbullets < MAX_BULLETS) {
-					Bullets *b = &g.barr[g.nbullets];
-					timeCopy(&b->time, &bt);
-					//Melanie: Enemy bullet skeleton.
-					b->pos[0] = enemy.pos[0];
-					b->pos[1] = enemy.pos[1];
-					b->vel[0] = enemy.vel[0];
-					b->vel[1] = enemy.vel[1];
-					//convert ship angle to radians
-					Flt rad = ((enemy.pos[0] +90.0) / 360.0f) * PI * 2.0;
-					//convert angle to a vector
-					Flt xdir = cos(rad);
-					Flt ydir = sin(rad);
-					b->pos[0] += xdir*20.0f;
-					b->pos[1] += ydir*20.0f;
-					b->vel[0] += xdir*6.0f + rnd()*0.1;
-					b->vel[1] += ydir*6.0f + rnd()*0.1;
-					b->color[0] = 1.0f;
-					b->color[1] = 1.0f;
-					b->color[2] = 1.0f;
-					++g.nbullets;
-				}
-			}
 		}
 		if (e->xbutton.button==3) {
 			//Right button is down
@@ -1072,9 +1044,11 @@ void physics()
 
     //Enemy: Ground and Bullet collison
     enemy.CollisonGround(ground);
+    enemy1.CollisonGround(ciel);
     enemy.movement(ground);
     for (int i = 0; i < player.mag; i++) {
     	enemy.CheckBullet(&player.ammo[i]);
+    	enemy1.CheckBullet(&player.ammo[i]);
 	
     }
 
@@ -1239,6 +1213,7 @@ void render()
     ggprint8b(&r, 16, c, "C - Credits and Controls");
 
     enemy.drawEnemy();
+    enemy1.drawEnemy();
 
     //Vec playerPos = {g.xres/2.0 , g.yres/2.0, 0.0};
     //Player player(100, playerPos);
